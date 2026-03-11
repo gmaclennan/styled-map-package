870687cea3173ba90e3f2d5470bd48ede33b6d77
Gregor MacLennan <gmaclennan@awana.digital>
2026-03-11 10:40:29 +0000

    update CI and vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,19 @@ import { fileURLToPath } from 'node:url'
 /** Files that are helpers/utilities, not test suites */
 const nonTestFiles = ['test/utils/**/*.js']
 
+/** @type {import('vitest/dist/node.js').BrowserInstanceOption[]} */
+const browserInstances = [{ browser: 'chromium' }]
+
+if (process.platform === 'darwin') {
+  browserInstances.push({ browser: 'webkit' })
+}
+
+if (process.platform !== 'win32') {
+  // Firefox tests keep timing out on Windows CI runners due to
+  // https://github.com/microsoft/playwright/issues/34586
+  browserInstances.push({ browser: 'firefox' })
+}
+
 export default defineConfig({
   test: {
     globalSetup: ['test/utils/global-setup.js'],
@@ -37,11 +50,7 @@ export default defineConfig({
             headless: true,
             provider: 'playwright',
             screenshotFailures: false,
-            instances: [
-              { browser: 'chromium' },
-              { browser: 'firefox' },
-              { browser: 'webkit' },
-            ],
+            instances: browserInstances,
             commands: {
               readdir: (await import('./test/utils/commands.js')).readdir,
               randomImage: (await import('./test/utils/commands.js'))
